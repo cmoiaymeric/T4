f2c7705ca0b62054e1a2b307a4ed1d094d3a3eab
[Ferencz] <[froudet]unistra.fr>
2026-04-08 15:55:27 +0200

    ecran d'accueil

--- civilization8/src/App.tsx
+++ civilization8/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from './components/ui/button'
+import { WordRotate } from "@/components/ui/word-rotate"
+
 
 function App() {
+    return(
+        <div className='flex flex-col items-center justify-center h-screen gap-8'>
+            <WordRotate className='flex flex-col items-center justify-center text-7xl font-bold' words={["Mobilization 8", "Civilization 8"]} />
+            <br></br>
+            <div className='flex flex-col items-center justify-center h-fit gap-4'>
+                <Button className='h-20 w-70 font-bold'>JOUER</Button>
+                <br></br>
+                <Button className='h-20 w-70 font-bold'>À propos</Button>
+            </div>
+            <br></br>
+            <footer className='flex flex-col items-center justify-center font-bold'>GROUPE: BRATISLAVA X RABAT X PARIS</footer>
+        </div>
+    )
 }
 
 export default App

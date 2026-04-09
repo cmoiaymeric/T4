28bb2e7aff2435a95af6d0cc10c38e92b841acb3
Aymeric Fenard <afenard.unistra.fr>
2026-04-09 10:01:17 +0200

    Fix css

--- mobilization/src/App.tsx
+++ mobilization/src/App.tsx
@@ -4,22 +4,25 @@ import { WordRotate } from "@/components/ui/word-rotate"
 import { NavLink } from 'react-router';
 
 function App() {
-
     return (
         <div className="App">
-            <div className='flex flex-col items-center justify-center h-screen gap-8'>
-                <WordRotate className='flex flex-col items-center justify-center text-7xl font-bold' words={["Mobilization", "Civilization 8"]} />
-                <br></br>
-                <div className='flex flex-col items-center justify-center h-fit gap-4'>
-                    <NavLink to="/game">
-                        <Button className='btn-jouer'>JOUER</Button>
-                    </NavLink>
-                    <br></br>
-                    <Button className='h-20 w-70 font-bold'>À propos</Button>
-                </div>
-                <br></br>
-                <footer className='flex flex-col items-center justify-center font-bold'>GROUPE: BRATISLAVA X RABAT X PARIS</footer>
+            <div className='menu-title-area'>
+                <WordRotate 
+                    className='flex flex-col items-center justify-center text-8xl font-bold' 
+                    words={["Mobilization", "Civilization 8"]} 
+                />
             </div>
+
+            <div className='menu-buttons-area'>
+                <NavLink to="/game" className="nav-link">
+                    <Button className='btn-jouer btn-large'>JOUER</Button>
+                </NavLink>
+                <NavLink to="/about" className="nav-link">
+                    <Button className='btn-about btn-small'>À propos</Button>
+                </NavLink>
+            </div>
+
+            <footer className='menu-footer'>GROUPE: BRATISLAVA X RABAT X PARIS</footer>
         </div>
     )
 }
